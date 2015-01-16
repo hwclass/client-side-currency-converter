ad265cd113ff8b3fb199f007172b8df318cbef55
hwclass <harderworking.class@gmail.com>
2015-01-16 12:26:28 +0200

    Added comments into the errorSection module.

--- app/public/js/modules/errorSection.js
+++ app/public/js/modules/errorSection.js
@@ -9,17 +9,26 @@
 
     return {
 
+      /**
+       * init is a method that is used to make the listeners add and start
+       */
     	init : function () {
     		this.$errorSection = sandbox.x('$')('#errorSection');
         this.listen();
     	},
 
+      /**
+       * listen is a method that is used to listen custom events throughtout the application for this module
+       */
     	listen : function () {
     		sandbox.listen('newDollarInputValue', this.newDollarInputValue, this);
     		sandbox.listen('newEuroInputValue', this.newEuroInputValue, this);
     		sandbox.listen('newYenInputValue', this.newYenInputValue, this);
     	},
 
+      /**
+       * newDollarInputValue is a method to listen to the entering keys in the Dollar currency input and present error.
+       */
     	newDollarInputValue : function (data) {
     		if (isNaN(data.value)) {
     			this.$errorSection.html(sandbox.x('config').ERROR.notNumeric);
@@ -28,6 +37,9 @@
     		}
     	},
 
+      /**
+       * newEuroInputValue is a method to listen to the entering keys in the Euro currency input and present error.
+       */
     	newEuroInputValue : function (data) {
     		if (isNaN(data.value)) {
     			this.$errorSection.html(sandbox.x('config').ERROR.notNumeric);
@@ -36,6 +48,9 @@
     		}
     	},
 
+      /**
+       * newYenInputValue is a method to listen to the entering keys in the Yen currency input and present error.
+       */
     	newYenInputValue : function (data) {
     		if (isNaN(data.value)) {
     			this.$errorSection.html(sandbox.x('config').ERROR.notNumeric);
