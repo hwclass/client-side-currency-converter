f62c59ffdb168684a0b476a74a5542c76f62accc
hwclass <harderworking.class@gmail.com>
2015-01-16 12:23:22 +0200

    Added comments into the currencyConverterForm.

--- app/public/js/modules/currencyConverterForm.js
+++ app/public/js/modules/currencyConverterForm.js
@@ -9,25 +9,40 @@
   
     return {
       
+      /**
+       * init is a method that is used to make the listeners add and start
+       */
       init: function() {
         this.$currencyConverterForm = sandbox.x('$')('#currencyConverterForm');
         this.listen();
       },
 
+      /**
+       * listen is a method that is used to listen custom events throughtout the application for this module
+       */
       listen: function() {
         sandbox.listen('newDollarInputValue', this.newDollarInputValue, this);
         sandbox.listen('newEuroInputValue', this.newEuroInputValue, this);
         sandbox.listen('newYenInputValue', this.newYenInputValue, this);
       },
 
+      /**
+       * newDollarInputValue is a method to listen to the entering keys in the Dollar currency input.
+       */
       newDollarInputValue : function (data) {
         //console.log('newDollarInputValue:', data.value);
       },
 
+      /**
+       * newEuroInputValue is a method to listen to the entering keys in the Euro currency input.
+       */
       newEuroInputValue : function (data) {
         //console.log('newEuroInputValue:', data.value);
       },
 
+      /**
+       * newYenInputValue is a method to listen to the entering keys in the Euro currency input.
+       */
       newYenInputValue : function (data) {
         //console.log('newYenInputValue', data.value);
       }
