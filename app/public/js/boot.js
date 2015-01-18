ee1b285aded52f5c03586273ff1c3a1bb657f23a
hwclass <harderworking.class@gmail.com>
2015-01-18 16:22:53 +0200

    Deleted the app.js and commented on the file, boot.js

--- app/public/js/boot.js
+++ app/public/js/boot.js
@@ -1,6 +1,11 @@
 'use strict';
 
 /*
+ * File :: boot.js
+ * Info : Code for initializing the modules. If you expect, you can set them one by one.
+ */
+
+/*
 Core.start('currencyConverterForm');
 Core.start('exchangeList');
 Core.start('amountOfDollar');
