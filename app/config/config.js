f3b74cb9b6b048bba6fd4de4ab1ce5a7fce0ba92
hwclass <harderworking.class@gmail.com>
2015-01-20 16:16:48 +0200

    Removed the paths for mongo database

--- app/config/config.js
+++ app/config/config.js
@@ -4,12 +4,12 @@ var rootPath = path.normalize(__dirname + '/../../');
 module.exports = {
   development: {
     rootPath: rootPath,
-    db: 'mongodb://bkm-admin:bkm123!@ds063769.mongolab.com:63769/bkm-event-tracker',
+    db: '',
     port: process.env.PORT || 8080
   },
   production: {
     rootPath: rootPath,
-    db: 'mongodb://bkm-admin:bkm123!@ds063769.mongolab.com:63769/bkm-event-tracker',
+    db: '',
     port: process.env.PORT || 8080
   }
 }
