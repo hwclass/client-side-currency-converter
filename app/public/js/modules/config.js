64ef9d27d82c5324ea2f48fc6a2731c438e150c1
hwclass <harderworking.class@gmail.com>
2015-02-10 15:45:11 +0200

    Edited indentation in the config.js

--- app/public/js/modules/config.js
+++ app/public/js/modules/config.js
@@ -3,28 +3,28 @@
  * Info : Module for the configuration constants.
  */
 
-  var config = (function(){ 
-    return {
-      CURRENCY : {
-      	DOLLAR : 'USD',
-      	EURO : 'EUR',
-      	YEN : 'JPY'
+var config = (function(){ 
+  return {
+    CURRENCY : {
+    	DOLLAR : 'USD',
+    	EURO : 'EUR',
+    	YEN : 'JPY'
+    },
+    API : {
+      CONVERSION : {
+        DOLLAR_TO_EURO : 'USD_EUR',
+        DOLLAR_TO_YEN : 'USD_JPY',
+        EURO_TO_DOLLAR : 'EUR_USD',
+        EURO_TO_YEN : 'EUR_JPY',
+        YEN_TO_DOLLAR : 'JPY_USD',
+        YEN_TO_EURO : 'JPY_EUR'
       },
-      API : {
-        CONVERSION : {
-          DOLLAR_TO_EURO : 'USD_EUR',
-          DOLLAR_TO_YEN : 'USD_JPY',
-          EURO_TO_DOLLAR : 'EUR_USD',
-          EURO_TO_YEN : 'EUR_JPY',
-          YEN_TO_DOLLAR : 'JPY_USD',
-          YEN_TO_EURO : 'JPY_EUR'
-        },
-        URL : {
-          CONVERT : 'http://www.freecurrencyconverterapi.com/api/v2/convert?q='
-        }
-      },
-      ERROR : {
-        notNumeric : 'Please, enter a numeric digit.'
+      URL : {
+        CONVERT : 'http://www.freecurrencyconverterapi.com/api/v2/convert?q='
       }
+    },
+    ERROR : {
+      notNumeric : 'Please, enter a numeric digit.'
     }
-  })();
+  }
+})();
