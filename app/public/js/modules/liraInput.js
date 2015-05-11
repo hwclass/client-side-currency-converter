f312f56a50352c8e4351f5674599c484fdbed6a1
hwclass <harderworking.class@gmail.com>
2015-05-11 16:58:49 +0300

    Added the Turkish Lira input module

---
+++ app/public/js/modules/liraInput.js
@@ -0,0 +1,16 @@
+/*
+ * Module :: liraInput.js
+ * Info : Module for the input element of lira currency.
+ */
+
+(function(Core) {
+	
+  Core.register('liraInput', function (sandbox) {
+
+    return {
+
+    }
+
+ 	});
+
+});
