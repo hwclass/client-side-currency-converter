8ba4f785e8c7b20beb0e6f864462605f0baf12a6
hwclass <harderworking.class@gmail.com>
2015-05-11 16:57:12 +0300

    Added the Turkish Lira input and settings.

--- app/public/js/modules/config.js
+++ app/public/js/modules/config.js
@@ -14,10 +14,13 @@ var config = (function(){
       CONVERSION : {
         DOLLAR_TO_EURO : 'USD_EUR',
         DOLLAR_TO_YEN : 'USD_JPY',
+        DOLLAR_TO_TRY : 'USD_TRY',
         EURO_TO_DOLLAR : 'EUR_USD',
         EURO_TO_YEN : 'EUR_JPY',
+        EURO_TO_TRY : 'EUR_TRY',
         YEN_TO_DOLLAR : 'JPY_USD',
-        YEN_TO_EURO : 'JPY_EUR'
+        YEN_TO_EURO : 'JPY_EUR',
+        YEN_TO_TRY : 'JPY_TRY'
       },
       URL : {
         CONVERT : 'http://www.freecurrencyconverterapi.com/api/v2/convert?q='
